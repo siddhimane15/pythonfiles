59bbc9df372d9bf94325e4128732d81077a26918
siddhimane15 <manesiddhi753@gmail.com>
2025-01-13 19:08:47 +0530

    new

---
+++ example1func.py
@@ -0,0 +1,25 @@
+
+def addition(x,y):
+
+    sum = x+y
+    return sum
+
+def subtraction(x,y):
+    ans = x-y
+    return ans
+
+def division(x,y):
+    ans2=x/y
+    return ans2
+
+def multiply(x,y):
+    ans =x*y
+    return ans
+
+x = 5
+y = 10
+
+ans = addition(x,y) + subtraction(x,y) + division(x,y)+multiply(x,y)
+
+print(ans)
+
