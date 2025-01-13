6bda6b413c210bfd34eaf1c12ce6b9dc237c9657
siddhimane15 <manesiddhi753@gmail.com>
2025-01-13 23:35:32 +0530

    new

---
+++ files.py
@@ -0,0 +1,18 @@
+#import os
+#print(os)
+#print(os.path)
+
+#if os.path.exit('demo.txt'):
+    #print("files exist")
+#else:
+    #print("files not exit")
+
+
+
+import datetime
+from datetime import datetime
+print(datetime.now()) 
+
+
+import calendar
+print(calendar.calendar(2025))
