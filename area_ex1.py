279aa07c149481ef4822356ef0783defeda3d9bb
siddhimane15 <manesiddhi753@gmail.com>
2025-01-17 19:29:35 +0530

    new

---
+++ area_ex1.py
@@ -0,0 +1,22 @@
+import math
+#pythagorus therom
+#area of circle 
+#area of triangle
+
+radius=8.5
+hypotenuse=2*radius
+adj_side=15
+front_side=math.sqrt((hypotenuse)**2)-(adj_side)**2
+print(front_side)
+
+
+area_of_circle=math.pi*radius**2
+print("area of circle:",area_of_circle)
+
+
+area_of_triangle=(1/2)*(adj_side)*(front_side)
+print("area of triangle:",area_of_triangle)
+
+
+c=area_of_circle-area_of_triangle
+print(c)
