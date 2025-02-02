d26a78c5714aa7ed984a521c33f6ff25b73920a7
siddhimane15 <159922228+siddhimane15@users.noreply.github.com>
2025-02-02 19:08:05 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,76 @@
+
+#assign and add
+x=1
+x+=9
+print(x)
+
+#assign and sub
+x=3
+x-=13
+print(x)
+
+#assign and mul
+x=3
+x*=15
+print(x)
+
+#assign and div
+x=5
+x/=15
+print(x)
+
+#assign and modulus
+x=8
+x%=15
+print(x)
+ 
+ #assign and expo
+x=5
+x**=15
+print(x)
+
+x=1
+x//=20
+print(x)
+
+
+#comparison operators
+
+y=4
+if y<5:
+    print('true')
+
+y=15
+if y>10:
+    print('true')
+
+y=10
+if y!=11:
+    print('true')
+
+#logical operator (and, or , not)
+    
+m=15
+if m==15 and m>10:
+    print('answers is correct')
+
+#bitwise operators (&, |, ^, ~)
+k=10&2 
+print(k)
+
+#identify operators (is, is not)
+
+name='siddhi'
+if name is 'siddhi':
+    print('true')
+    if name is not 'rishi':
+        print('false')
+
+
+#membership (in, not in)
+
+        
+
+
+
+
