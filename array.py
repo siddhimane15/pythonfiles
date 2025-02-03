5d8e0cae859b29f510d73fe248c11383716e182c
siddhimane15 <159922228+siddhimane15@users.noreply.github.com>
2025-02-03 11:20:00 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,21 @@
+import  numpy as np
+
+matrix_a = np.array([[8,15],[7,2]])
+matrix_b = np.array([[18,26],[67,90]])
+
+result =np.add(matrix_a,matrix_b)
+print(result)
+
+#example 2
+matrix_s=np.array([[1,2,3],[3,4,5],[89,67,80]])
+matrix_h=np.array([[7,8,9],[15,16,17],[13,15,67]])
+
+result2=np.add(matrix_s,matrix_h)
+print(result2)
+
+#example 3
+matrix_p=np.array([[8,6],[5,9]])
+matrix_f=np.array([[9,6],[6,9]])
+
+result3=np.subtract(matrix_p,matrix_f)
+print(result3)
